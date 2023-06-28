e0fd8e19403d155f9581f3d4e6262374013c5729
Juan <jm.arango03@gmail.com>
2023-06-28 16:45:50 -0500

    Initial setup

---
+++ src/test/java/com/adminuniversity/adminuniversityrest/AdminUniversityRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adminuniversity.adminuniversityrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminUniversityRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
