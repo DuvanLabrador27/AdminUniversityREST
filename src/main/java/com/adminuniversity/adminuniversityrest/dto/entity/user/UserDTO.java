6239aa05c201234275fa58621f77015602feaf4c
Juan <jm.arango03@gmail.com>
2023-06-29 12:22:30 -0500

    Changed UserDTO to abstract class

--- src/main/java/com/adminuniversity/adminuniversityrest/dto/entity/user/UserDTO.java
+++ src/main/java/com/adminuniversity/adminuniversityrest/dto/entity/user/UserDTO.java
@@ -3,7 +3,7 @@ package com.adminuniversity.adminuniversityrest.dto.entity.user;
 import lombok.Data;
 
 @Data
-public class UserDTO {
+public abstract class UserDTO {
     protected Long id;
     protected String email;
     protected String firstName;
